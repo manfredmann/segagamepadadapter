01ac5c303b15fb1aa6127dde2f675311a8152848
Roman G. Serov <roman@serov.co>
2018-10-28 10:27:54 +0300

    Добавил заголовки лицензии

--- src/list.h
+++ src/list.h
@@ -1,3 +1,22 @@
+/*
+ * This file is part of the SEGA Gamepad adapter project.
+ *
+ * Copyright (C) 2018 Roman Serov <roman@serov.co>
+ *
+ * SEGA Gamepad adapter is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SEGA Gamepad adapter is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SEGA Gamepad adapter.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef LIST_H
 #define LIST_H
 
